099b3511285bb01718bc477a4aeead6382aa81f7
Cailley LaPara <cailley.lapara67@journalism.cuny.edu>
2022-11-01 19:44:50 -0400

    YT API implemented

--- main.js
+++ main.js
@@ -2,9 +2,62 @@ const video_frame = document.querySelector(".video-frame");
 const watch = new Watch(video_frame, {
     threshold: 0.75,
 });
+let player;
+
+function onYouTubeIframeAPIReady() {
+    const video_player = document.querySelector(".yt-player");
+    const video_id = video_player.dataset.videoId;
+    player = new YT.Player(video_player, {
+      height: "390",
+      width: "640",
+      videoId: video_id,
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        'onStateChange': onPlayerStateChange
+      }
+    });
+}
+
+function onPlayerStateChange(e) {
+
+    if(e.data === YT.PlayerState.PLAYING) {
+        video_frame.classList.add("can-stick")
+    } else {
+        video_frame.classList.remove("can-stick")
+    }
+}
 
 watch.inView(() => {
     video_frame.classList.remove("sticky");
 }).outView(() => {
     video_frame.classList.add("sticky");
 });
+
+
+function goToChapter(timecode) {
+    console.log(">> TIMECODE >>", timecode);
+    timecode = timecode.split(":");
+    const seconds = (parseInt(timecode[0]) * 60 * 60) + 
+    (parseInt(timecode[1]) * 60) + 
+    (parseInt(timecode[2]));
+
+    console.log(">> SECONDS >>", seconds);
+
+    player.playVideo();
+    player.seekTo(seconds, true);
+    
+}
+
+const chapters = document.querySelectorAll(".chapters li a");
+
+chapters.forEach((chapter) => {
+    const timecode = chapter.closest("li").dataset.timecode;
+    chapter.addEventListener("click", function (e) {
+        e.preventDefault();
+        goToChapter(timecode);
+    });
+});
+
+
